b56bc44c3165a6bed15c46c80c61ad3971d6001d
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2025-07-10 22:14:50 +0300

    feat: enhance celery task error handling and add time limit

--- reports/tasks.py
+++ reports/tasks.py
@@ -7,6 +7,7 @@ from datetime import date, timedelta
 
 import pandas as pd  # type: ignore[import-untyped]
 from celery import shared_task  # type: ignore[import-untyped]
+from celery.exceptions import MaxRetriesExceededError  # type: ignore[import-untyped]
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
@@ -24,8 +25,8 @@ def debug_celery_task() -> str:
     return "Success"
 
 
-@shared_task(rate_limit="10/m")  # type: ignore[misc]
-def generate_report(report_id: str) -> None:
+@shared_task(rate_limit="10/m", bind=True, max_retries=3, default_retry_delay=1 * 60)  # type: ignore[misc]
+def generate_report(self, report_id: str) -> None:  # type: ignore
     start = time.time()
     _logger.info(f"Generating report for ID: {report_id}")
     try:
@@ -91,9 +92,19 @@ def generate_report(report_id: str) -> None:
         report.save()
         msg = f"✅ Report generated in {time.time() - start} seconds"
         _logger.info(msg)
-    except Exception as e:
-        report.status = Report.Status.FAILED
-        report.error = str(e)
-        report.save()
-        msg = f"❌ Failed to generate report: {str(e)}. After: {time.time() - start} seconds"
+    except Report.DoesNotExist:
+        msg = f"Report {report_id} does not exist."
         _logger.error(msg)
+
+    except Exception as e:
+        _logger.exception(f"Failed to generate report {report_id}: {e}")
+        try:
+            self.retry(
+                exc=e,
+            )
+        except MaxRetriesExceededError:
+            report = Report.objects.get(id=report_id)
+            if report:
+                report.status = Report.Status.FAILED
+                report.error = str(e)
+                report.save()
